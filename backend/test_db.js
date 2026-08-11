dd3fc8403b9fd5e9c591ff2cd8a3f5610a82659c
Alberto García Mendoza <alberto.garmdz@gmail.com>
2026-08-11 12:34:32 -0600

    clean: remove junk, temporary, and test scripts from repository

--- backend/test_db.js
+++
@@ -1,7 +0,0 @@
-const { pool } = require('./config/pg-db');
-async function test() {
-  const res = await pool.query("SELECT column_name FROM information_schema.columns WHERE table_name = 'sap_incoming_payments'");
-  console.log(res.rows.map(x=>x.column_name));
-  process.exit(0);
-}
-test().catch(e=>console.error(e));
