b26ac518d309a2de728e44f7978c81aeb53efa91
Alberto García Mendoza <alberto.garmdz@gmail.com>
2026-08-26 10:21:12 -0600

    chore: limpieza de scripts de prueba y parches temporales

--- backend/write_test.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-const code = fs.readFileSync('routes/export.routes.js', 'utf8');
-const exec = "require('dotenv').config();\n" + code.replace(/module\.exports[\s\S]*/, '') + "\nresolveReportData('consulta-externa', {}).then(res => console.log('OK', res.titulo, res.columnas.length)).catch(console.error).finally(()=>process.exit(0));";
-fs.writeFileSync('test_export2.js', exec);
